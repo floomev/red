4c9977474624226b93ba47644b1a5863e5bf1b63
floome <vedy2018@gmail.com>
2023-10-12 17:15:48 +0700

    red-square

---
+++ req-square/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<template>
+  <div :style="{display: 'flex', justifyContent:'space-evenly' , alignItems: 'center',  alignContent:'center', height: '100vh', }">
+    <div class="square"></div>
+  </div>
+  
+</template>
+
+<style>
+.square {
+  
+width: 40px;
+height: 40px;
+background-color: red;
+border: 1px solid black;
+}
+
+</style>
